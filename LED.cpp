9e1370c969feff6e700254758a6d5c21deff8050
Paul Bininda <paul@bininda.com>
2017-02-19 09:29:55 +0100

    integrate NeoPixelBus library in an attempt to fix flickering

--- LED.cpp
+++ LED.cpp
@@ -1,4 +1,14 @@
+#define BUS
+// GPIO3 UART RXD0
+//#define BUS_METHOD NeoEsp8266Dma800KbpsMethod
+// GPIO2 UART TXD1
+#define BUS_METHOD NeoEsp8266Uart800KbpsMethod
+
+#ifdef BUS
+#include <NeoPixelBus.h>
+#else
 #include <Adafruit_NeoPixel.h>
+#endif
 #include "LED.h"
 #include "state.h"
 
@@ -6,7 +16,14 @@ const int DEBUG_LED = 0;
 const int DEBUG_LED_NO_OUT = 0;
 const int NUM_LED_DEBUG = 32;
 const int LED_PIN = 2;
+#ifdef BUS
+NeoPixelBus<NeoGrbFeature, BUS_METHOD> strip(NUM_LEDS, LED_PIN);
+#define canShow CanShow
+#define begin Begin
+#define show Show
+#else
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+#endif
 
 bool ledsChanged = false;
 time_t lastLedChange = 0;
@@ -15,25 +32,53 @@ time_t getLastLedChangeDelta() {
   return state.now - lastLedChange;
 }
 
+#ifdef BUS
+uint32_t ledColor(uint8_t r, uint8_t g, uint8_t b) {
+  return (((r << 8) | g) << 8) | b;
+}
+
+uint32_t getLed(uint16_t n) {
+  RgbColor co = strip.GetPixelColor(n);
+  return ledColor(co.R, co.G, co.B);
+}
+#else
 uint32_t ledColor(uint8_t r, uint8_t g, uint8_t b) {
   return Adafruit_NeoPixel::Color(r, g, b);
 }
 
+uint32_t getLed(uint16_t n) {
+  return strip.getPixelColor(n);
+}
+#endif
+
 uint32_t ledBriColor(uint16_t bri, uint8_t r, uint8_t g, uint8_t b) {
   uint32_t br = bri;
-  return Adafruit_NeoPixel::Color(r * br / 256, g * br / 256, b * br / 256);
+  return ledColor(r * br / 256, g * br / 256, b * br / 256);
 }
 
-void setLed(uint8_t n, uint32_t c) {
-  uint32_t oldC = strip.getPixelColor(n);
+
+void setLed(uint16_t n, uint32_t c) {
+  uint32_t oldC = getLed(n);
   if (oldC != c) {
     lastLedChange = state.now;
     ledsChanged = true;
+#ifdef BUS
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    b = c & 255;
+    c >>= 8;
+    g = c & 255;
+    c >>= 8;
+    r = c & 255;
+    strip.SetPixelColor(n, RgbColor(r, g, b));
+#else
     strip.setPixelColor(n, c);
+#endif
   }
 }
 
-void setLed(uint8_t n, uint8_t r, uint8_t g, uint8_t b) {
+void setLed(uint16_t n, uint8_t r, uint8_t g, uint8_t b) {
   setLed(n, ledColor(r, g, b));
 }
 
@@ -42,7 +87,7 @@ bool sendLeds() {
     if (DEBUG_LED) {
       Serial.print("LEDs: ");
       for (uint16_t i = 0; i < NUM_LED_DEBUG; i++) {
-        Serial.print(strip.getPixelColor(i) % 256 , HEX);
+        Serial.print(getLed(i) % 256 , HEX);
         Serial.print(" ");
       }
       Serial.println();
--- LED.h
+++ LED.h
@@ -5,8 +5,8 @@ const uint16_t NUM_LEDS = 105;
 
 extern uint32_t ledColor(uint8_t r, uint8_t g, uint8_t b);
 extern uint32_t ledBriColor(uint16_t bri, uint8_t r, uint8_t g, uint8_t b);
-extern void setLed(uint8_t n, uint8_t r, uint8_t g, uint8_t b);
-extern void setLed(uint8_t n, uint32_t c);
+extern void setLed(uint16_t n, uint8_t r, uint8_t g, uint8_t b);
+extern void setLed(uint16_t n, uint32_t c);
 extern bool sendLeds();
 extern time_t getLastLedChangeDelta();
 
