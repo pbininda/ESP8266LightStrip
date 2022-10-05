e03f6be2bac9d2590c7a9b552c408997705a7a65
Paul Bininda <paul@bininda.com>
2022-10-05 14:23:42 +0200

    127 LEDs in badoben

--- LED.h
+++ LED.h
@@ -1,7 +1,7 @@
 #ifndef _LED_H_
 #define _LED_H_
 
-const uint16_t NUM_LEDS = 28;
+const uint16_t NUM_LEDS = 127;
 
 extern uint32_t ledColor(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
 extern uint32_t ledBriColor(uint16_t bri, uint8_t r, uint8_t g, uint8_t b);
