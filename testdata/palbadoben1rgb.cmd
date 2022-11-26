8493fc09a671a67837a7fbe8ac173134512596d6
Paul Bininda <paul@bininda.com>
2022-11-26 21:50:46 +0100

    ad script for primary color setup

---
+++ testdata/palbadoben1rgb.cmd
@@ -0,0 +1,8 @@
+call .\setpal.cmd espbadoben1 0 255 255 255
+call .\setpal.cmd espbadoben1 1 255 0 0
+call .\setpal.cmd espbadoben1 2 0 255 0
+call .\setpal.cmd espbadoben1 3 0 0 255
+call .\setpal.cmd espbadoben1 4 255 255 0
+call .\setpal.cmd espbadoben1 5 255 0 255
+call .\setpal.cmd espbadoben1 6 0 255 255
+call .\setpal.cmd espbadoben1 7 255 128 128
